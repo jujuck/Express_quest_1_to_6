261f002375c4a20e6ab952b1869103edc0bd0858
Julien ichard WCS <julien.y.richard@gmail.com>
2021-12-01 15:55:09 +0100

    Quest 10: bonus adding invlaid email and password

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,19 @@ authRouter.get('/checkCredentials', (req, res) => {
   const { email, password } = req.body;
   Users.findPasswordFromEmail(email)
     .then((results) => {
-      console.log(results[0][0].hashedPassword)
-      Users.verifyPassword(password, results[0][0].hashedPassword)
-        .then((results) => {
-          console.log(results)
-          res.send('User credentials are valid')
-        })
+      console.log(results[0][0])
+      if (!results[0][0]) {
+        res.status(401).send('Invalid Credential')
+      } else {
+        Users.verifyPassword(password, results[0][0].hashedPassword)
+          .then((results) => {
+            if (results) {
+              res.status(200).send('User credentials are valid')
+            } else {
+              res.status(401).send('Invalid Credential')
+            }
+          })
+      }
     })
     .catch((err) => console.error(err))
 })
